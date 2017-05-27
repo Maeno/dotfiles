5053f89acbaf42f039b0d6d30a7fead7ad58e768
前野大輔 <maenodaisuke@maenodaisukenoMacBook-Pro.local>
2017-05-27 21:53:40 +0900

    add configuration for skk on mac.

--- emacs/init.el
+++ emacs/init.el
@@ -64,7 +64,26 @@
   (setq ns-alternate-modifier (quote super))
   )
 
+;; skk
+(when (eq system-type 'darwin)
+  (require 'skk)
 
+  ;;skk-server AquaSKK
+  (setq skk-server-portnum 1178)
+  (setq skk-server-host "localhost")
 
+  (global-set-key "\C-x\C-j" 'skk-mode)
+
+  (add-hook 'isearch-mode-hook
+	    (function (lambda ()
+			(and (boundp 'skk-mode) skk-mode
+			     (skk-isearch-mode-setup)))))
+  (add-hook 'isearch-mode-end-hook
+	    (function (lambda ()
+			(and (boundp 'skk-mode) skk-mode (skk-isearch-mode-cleanup))
+			(and (boundp 'skk-mode-invoked) skk-mode-invoked
+			     (skk-set-cursor-properly)))))
+  (setq mac-pass-control-to-system nil)
+  )
 
 
